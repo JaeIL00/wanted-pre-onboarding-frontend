d92c3cef6668e5ec9920d03b3a20f6e978ab49af
ShinJaeIl01 <ekvm555@naver.com>
2023-08-05 15:15:52 +0900

    feat: 로그인 페이지 로직 구현

---
+++ src/components/signin/index.jsx
@@ -0,0 +1,76 @@
+import { useCallback, useState } from "react";
+import "./style.scss";
+import debounce from "../../utils/debounce";
+import { useNavigate } from "react-router-dom";
+
+const SignIn = () => {
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isEmailPassed, setIsEmailPassed] = useState(false);
+    const [isPasswordPassed, setIsPasswordPassed] = useState(false);
+
+    const onChangeEmail = (event) => {
+        setEmail(event.target.value);
+        emailValidation(event.target.value);
+    };
+    const emailValidation = useCallback(
+        debounce((email) => {
+            const isPassed = email.includes("@");
+            if (isPassed) setIsEmailPassed(true);
+            else setIsEmailPassed(false);
+        }, 500),
+        []
+    );
+
+    const onChangePassword = (event) => {
+        setPassword(event.target.value);
+        passwordValidation(event.target.value);
+    };
+    const passwordValidation = useCallback(
+        debounce((password) => {
+            const isPassed = password.length >= 8;
+            if (isPassed) setIsPasswordPassed(true);
+            else setIsPasswordPassed(false);
+        }, 500),
+        []
+    );
+
+    const signInHandler = () => {
+        navigate("/todo");
+    };
+
+    return (
+        <main className="container">
+            <header>
+                <h1 className="title">로그인</h1>
+            </header>
+            <form className="formContainer">
+                <input
+                    type="text"
+                    value={email}
+                    onChange={onChangeEmail}
+                    placeholder="이메일 입력"
+                    data-testid="email-input"
+                />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={onChangePassword}
+                    placeholder="비밀번호 입력"
+                    data-testid="password-input"
+                />
+                <button
+                    onClick={signInHandler}
+                    disabled={!(isEmailPassed && isPasswordPassed)}
+                    data-testid="signin-button"
+                >
+                    로그인
+                </button>
+            </form>
+        </main>
+    );
+};
+
+export default SignIn;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./home";
+import SignInPage from "./signin";
 
 const RootRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/signin" element={<SignInPage />} />
         </Routes>
     );
 };
