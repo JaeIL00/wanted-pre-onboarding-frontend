d4d4d517018f54ccb1d10d45802bb05f52a689ae
ShinJaeIl01 <ekvm555@naver.com>
2023-08-06 16:58:13 +0900

    feat: 투두 추가 기능 구현

--- src/components/todo/index.jsx
+++ src/components/todo/index.jsx
@@ -1,14 +1,49 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLayoutEffect, useState } from "react";
+import "./style.scss";
+import axios from "axios";
 
-const Todo = () => {
-    const navigate = useNavigate();
+const Todo = ({ accessToken }) => {
+    const [addTodoText, setAddTodoText] = useState("");
+    const [todoList, setTodoList] = useState([]);
 
-    useEffect(() => {
-        const accessToken = localStorage.getItem("access_token");
-        if (!accessToken) navigate("/signin");
-    }, []);
-    return <main></main>;
+    // Add todo
+    const onChangeTodoText = (event) => {
+        setAddTodoText(event.target.value);
+    };
+    const onClickAddButton = async () => {
+        const response = await axios({
+            baseURL: "https://www.pre-onboarding-selection-task.shop",
+            url: "/todos",
+            method: "POST",
+            headers: {
+                ContentType: "application/json",
+                Authorization: `Bearer ${accessToken}`,
+            },
+            data: { todo: addTodoText },
+        });
+        console.log(response);
+        if (response.status === 201) {
+            setTodoList([...todoList, response.data]);
+        }
+    };
+
+    return (
+        <main className="container">
+            <section>
+                <input
+                    value={addTodoText}
+                    onChange={onChangeTodoText}
+                    data-testid="new-todo-input"
+                />
+                <button
+                    onClick={onClickAddButton}
+                    data-testid="new-todo-add-button"
+                >
+                    추가
+                </button>
+            </section>
+        </main>
+    );
 };
 
 export default Todo;
--- src/pages/todo/index.jsx
+++ src/pages/todo/index.jsx
@@ -1,7 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Todo from "../../components/todo";
 
 const TodoPage = () => {
-    return <Todo />;
+    const navigate = useNavigate();
+
+    const [accessToken, setAccessToken] = useState("");
+
+    useEffect(() => {
+        const accessTokenStorage = localStorage.getItem("access_token");
+        if (!accessTokenStorage) return navigate("/signin");
+        setAccessToken(accessTokenStorage);
+    }, []);
+
+    return <Todo accessToken={accessToken} />;
 };
 
 export default TodoPage;
